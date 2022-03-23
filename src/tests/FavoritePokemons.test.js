8a028b0e4c3e0efc182584b60d31410a4137f590
erik-efl <erik.lima.f@outlook.com>
2022-03-23 13:34:22 -0300

    Adicionando requisito 3 e 4 :mouse::heart::rocket:

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testando a pagina ', () => {
+  it('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textContains = screen.getByText(/No favorite pokemon found/i);
+    expect(textContains).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    /*     const cards = screen.getByRole('img', { name: 'is marked as favorite' });
+    expect(cards.src).toStrictEqual('/star-icon.svg'); */
+    const cards = screen.getAllByTestId('pokemon-name');
+    expect(cards).toHaveLength(1);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it(`Teste se página contém um heading h2 com o texto 
+  Page requested not found 😭`, () => {
+    renderWithRouter(<NotFound />);
+
+    const haveTheText = screen.getByRole('heading', { level: 2 },
+      'Page requested not found 😭');
+    expect(haveTheText).toBeInTheDocument();
+  });
+
+  it(`Teste se página mostra a imagem 
+  https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
+    renderWithRouter(<NotFound />);
+
+    const haveImage = screen.getByAltText(/Pikachu crying because the page/i);
+    expect(haveImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
